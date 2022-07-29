c9e15cfe75a1644f0f66b9a1f0f55d8c723d504b
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-29 17:30:37 +0500

    Update spreadInObjLitrelas.js

--- spreadInObjLitrelas.js
+++ spreadInObjLitrelas.js
@@ -1,2 +1,26 @@
 // now lets take a look on spread in object literals.
 
+// Used to copy the property of one object into a new object. ahh we can copy objects, we can combine objects, we can add objects. 
+// lets take some exapmles for better understanding.
+
+const student = {
+    Name : "Muhammad Shakir",
+    Age  : 21,
+    Gender : "Male",
+}
+
+const socialDetails = {
+    email : "chaudhryshakir672gmail.com",
+    github : "MuhammadShakir-dev",
+}
+
+const residence = {
+    city : "Karachi",
+    country : "Pakistan",
+};
+
+const allStudentDetails = {...student, ...socialDetails, ...residence};
+
+console.log(allStudentDetails);
+
+
