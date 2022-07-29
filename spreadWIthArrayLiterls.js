3673433c517a1e8d4f21746a66207ac2cab28175
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-29 17:13:52 +0500

    Create spreadWIthArrayLiterls.js

---
+++ spreadWIthArrayLiterls.js
@@ -0,0 +1,10 @@
+// spread is also used to combine to or more array into a single array and you can add any thing inside of it basically it just make a copy of an actual aray.
+
+
+
+const gdsc22 = ["Danella","Mahnoor","Urwa","Arisha","Azhar","Osama","Usman"];
+
+const gdsc23 = ["Wajid","Shakir","Anum","Shiza","Aisha","Mudasir","Ammad","Mehfooz","Zaviyar"];
+
+const sumUp = ["Lead 2022",...gdsc22, "Lead 2023", ...gdsc23];
+console.log(sumUp);
