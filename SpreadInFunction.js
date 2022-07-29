e398ffb6244962a49144b19f6079be9e41e4bc9c
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-29 16:59:16 +0500

    Update SpreadInFunction.js

--- SpreadInFunction.js
+++ SpreadInFunction.js
@@ -1,1 +1,22 @@
+// Note use Math.max and Math.min
 
+//Spread Function spread the array into individual singel arguments 
+// representation.
+
+// ...   thi is the representation of spread function.
+
+// now lets practice some code.
+
+const numbers = [19.77,18.33,32.43,99.99,799,652,753,892,1765];
+// find the maximum number.
+console.log(numbers);
+
+const maxNum = Math.max(...numbers);
+console.log("Maximum Number");
+console.log(maxNum);
+
+//find the minimum number from this array.
+
+const minNum = Math.min(...numbers);
+console.log("Minimun Number");
+console.log(minNum);
