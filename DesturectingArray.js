7a5ca0362f9b181eff81f444bb458ef429ebcce6
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-31 16:28:24 +0500

    Update DesturectingArray.js

--- DesturectingArray.js
+++ DesturectingArray.js
@@ -23,3 +23,12 @@ const ary = [77,31,108,107];
 const [Shakir,Anum,Sunila,Shiza] = ary;
 console.log("This is the roll no of Shakir : " +  Shakir);
 console.log("This is the roll no of Anum : " +  Anum);
+
+
+// use Rest Parameter inside of it.
+const ary = [77,31,108,107];
+
+const [Shakir,Anum,...everyOne] = ary;
+console.log("This is the roll no of Shakir : " +  Shakir);
+console.log("This is the roll no of Anum : " +  Anum);
+console.log("The remaning students are :  " + everyOne);
