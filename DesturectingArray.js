d9c3aaeb71d0f39ead5164162a38320b5395bdef
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-31 16:25:03 +0500

    Create DesturectingArray.js

---
+++ DesturectingArray.js
@@ -0,0 +1,25 @@
+// Desturecting
+
+// A short clean syntax to `unpack`;
+// values from array;
+// properties from objects;
+// into distinct variable;
+
+
+//to do this previously we used syntax like this.
+
+const ary = [77,31,108,107];
+
+const highScore = ary[2];
+const lowestScore =ary[1];
+
+console.log(highScore + " " + lowestScore);
+
+
+// the nwer way to do this.
+
+const ary = [77,31,108,107];
+
+const [Shakir,Anum,Sunila,Shiza] = ary;
+console.log("This is the roll no of Shakir : " +  Shakir);
+console.log("This is the roll no of Anum : " +  Anum);
